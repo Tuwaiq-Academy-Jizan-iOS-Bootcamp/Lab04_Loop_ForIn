6bae543eede8d47e3a9637218d6a0f70f483b666
hamad <7amad7arisi@gmail.com>
2021-10-12 21:55:18 +0300

    lab04

---
+++ lab04.playground/Contents.swift
@@ -0,0 +1,30 @@
+/*let myNumber = 3
+
+for _ in 1...5 {
+myNumber %  0 && myNumber % 7 == 0
+
+print(myNumber)}
+*/
+
+/*let myNumber = 3
+
+if (myNumber %= 11)
+{
+    print(myNumber)
+}
+
+
+let myNumber =
+
+for _ in 1...5 {
+
+if myNumber.isMultiple(of: 6)
+{
+    print(myNumber)
+}
+    else
+    {print(myNumber)
+    
+}
+}
+*/
