aaa7d5e47d43e0db4cabdfef4f6220618b2f6db2
grand ahmad <grand23@grands-MacBook-Pro.local>
2021-10-13 09:29:21 +0300

    atyaf

--- Loop4.playground/Contents.swift
+++ Loop4.playground/Contents.swift
@@ -1,5 +1,24 @@
-for i in 1...100{
+/*for i in 1...100{
     if ((i % 3) == 0){
-        print(i)
+//        print(i)
     }
 }
+*/
+var i = 1
+for i in 1...100{
+    if i.isMultiple(of: 3){
+        print(i)
+    }
+    else{
+    continue
+    break
+    }
+}
+
+/*var i = 1
+        for i in 1...100{
+    if i.isMultiple(of: 3){
+        print(i)
+    }
+    break
+        }*/
